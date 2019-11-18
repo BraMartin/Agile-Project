a281cc4682bfcd969810262221a987d1f75937e8
BraMartin <57913286+BraMartin@users.noreply.github.com>
2019-11-18 19:36:06 +0100

    Update Firebase.js

--- SocialStudent/app/modules/Firebase.js
+++ SocialStudent/app/modules/Firebase.js
@@ -9,13 +9,6 @@ import "firebase/firestore"
 // console.log(firebase.app())
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBhNyBXa0WBxhEpBBOy5sOngZrhgH09xbU",
-    authDomain: "studentsocial-f7e69.firebaseapp.com",
-    databaseURL: "https://studentsocial-f7e69.firebaseio.com",
-    projectId: "studentsocial-f7e69",
-    storageBucket: "studentsocial-f7e69.appspot.com",
-    messagingSenderId: "398762582509",
-    appId: "1:398762582509:web:0b7af139a7623517"
 };
 //initializeApp
 
